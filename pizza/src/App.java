a4411be8d4835b69dd2695f12527dd727b90c24f
xieyukun2005 <3327238085@qq.com>
2025-03-03 19:44:36 +0800

    Add files via upload

---
+++ pizza/src/App.java
@@ -0,0 +1,8 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        Slice_o_Heaven PIZZA = new Slice_o_Heaven();
+        
+        PIZZA.takeOrder("1", "2 superbig banana pizza", 2.00);
+        PIZZA.makepizza("1", "2 superbig banana pizza", 2.00);
+    }
+}
